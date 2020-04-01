969bedd099d202c42e426d4103ad74f2319bdc6f
Vitalii Shapoval <vitalii.shapoval@starnavi.io>
2020-04-01 22:05:52 +0300

    feat: wip lab2 dots

--- src/pathCtx.ts
+++ src/pathCtx.ts
@@ -1,5 +1,7 @@
 import * as R from 'ramda';
 
+import { INITIAL_X, INITIAL_Y } from './constants';
+
 import {
   drawXAxis,
   drawYAxis,
@@ -70,93 +72,196 @@ const buildPath = () => {
   ctx.stroke();
   ctx.strokeStyle = 'black';
 
-  const doorDots = [
-    {
-      x: 220,
-      y: 370,
-    },
-    {
-      x: 270,
-      y: 370,
-    },
-    {
-      x: 270,
-      y: 270,
-    },
-    {
-      x: 220,
-      y: 270,
-    },
-    {
-      x: 220,
-      y: 220,
-    },
-  ];
-  const wheelDots = [
+  const arr = [
     [
-      {
-        x: 270,
-        y: 320,
-      },
-      {
-        x: 320,
-        y: 320,
-      },
-      {
-        x: 320,
-        y: 370,
-      },
+      { x: 24, y: 116 },
+      { x: 83.7, y: 121 },
+      { x: 143.2, y: 127.8 },
     ],
     [
-      {
-        x: 320,
-        y: 370,
-      },
-      {
-        x: 320,
-        y: 420,
-      },
-      {
-        x: 270,
-        y: 420,
-      },
-    ],
-    [
-      {
-        x: 270,
-        y: 420,
-      },
-      {
-        x: 220,
-        y: 420,
-      },
-      {
-        x: 220,
-        y: 370,
-      },
-    ],
-    [
-      {
-        x: 220,
-        y: 370,
-      },
-      {
-        x: 220,
-        y: 320,
-      },
-      {
-        x: 270,
-        y: 320,
-      },
+      { x: 143.2, y: 127.8 },
+      { x: 152.3, y: 140.9 },
+      { x: 167.9, y: 144.1 },
     ],
   ];
 
-  const curveWheelDots = wheelDots.map(dots => getCurveDots(dots[0], dots[1], dots[2]));
+  const curvedDots = arr
+    .map(dots => dots.map(dot => ({
+      x: dot.x + INITIAL_X,
+      y: canvasHeight - dot.y - INITIAL_Y,
+    })))
+    .map(dots => getCurveDots(dots[0], dots[1], dots[2]));
 
   ctx.beginPath();
-  wheelDots.map(build(ctx));
-  curveWheelDots.map(build(ctx));
+  curvedDots.map(build(ctx));
   ctx.stroke();
+
+  // const doorDots = [
+  //   {
+  //     x: 220,
+  //     y: 370,
+  //   },
+  //   {
+  //     x: 270,
+  //     y: 370,
+  //   },
+  //   {
+  //     x: 270,
+  //     y: 270,
+  //   },
+  //   {
+  //     x: 220,
+  //     y: 270,
+  //   },
+  //   {
+  //     x: 220,
+  //     y: 220,
+  //   },
+  // ];
+  // const wheelDots1 = [
+  //   [
+  //     {
+  //       x: 470,
+  //       y: 320,
+  //     },
+  //     {
+  //       x: 520,
+  //       y: 320,
+  //     },
+  //     {
+  //       x: 520,
+  //       y: 370,
+  //     },
+  //   ],
+  //   [
+  //     {
+  //       x: 520,
+  //       y: 370,
+  //     },
+  //     {
+  //       x: 520,
+  //       y: 420,
+  //     },
+  //     {
+  //       x: 470,
+  //       y: 420,
+  //     },
+  //   ],
+  //   [
+  //     {
+  //       x: 470,
+  //       y: 420,
+  //     },
+  //     {
+  //       x: 420,
+  //       y: 420,
+  //     },
+  //     {
+  //       x: 420,
+  //       y: 370,
+  //     },
+  //   ],
+  //   [
+  //     {
+  //       x: 420,
+  //       y: 370,
+  //     },
+  //     {
+  //       x: 420,
+  //       y: 320,
+  //     },
+  //     {
+  //       x: 470,
+  //       y: 320,
+  //     },
+  //   ],
+  // ];
+  // const wheelDots2 = [
+  //   [
+  //     {
+  //       x: 70,
+  //       y: 320,
+  //     },
+  //     {
+  //       x: 120,
+  //       y: 320,
+  //     },
+  //     {
+  //       x: 120,
+  //       y: 370,
+  //     },
+  //   ],
+  //   [
+  //     {
+  //       x: 120,
+  //       y: 370,
+  //     },
+  //     {
+  //       x: 120,
+  //       y: 420,
+  //     },
+  //     {
+  //       x: 70,
+  //       y: 420,
+  //     },
+  //   ],
+  //   [
+  //     {
+  //       x: 70,
+  //       y: 420,
+  //     },
+  //     {
+  //       x: 20,
+  //       y: 420,
+  //     },
+  //     {
+  //       x: 20,
+  //       y: 370,
+  //     },
+  //   ],
+  //   [
+  //     {
+  //       x: 20,
+  //       y: 370,
+  //     },
+  //     {
+  //       x: 20,
+  //       y: 320,
+  //     },
+  //     {
+  //       x: 70,
+  //       y: 320,
+  //     },
+  //   ],
+  // ];
+
+  // const dots = [
+  //   {
+  //     x: 420,
+  //     y: 370,
+  //   },
+  //   {
+  //     x: 250,
+  //     y: 360,
+  //   },
+  //   {
+  //     x: 70,
+  //     y: 300,
+  //   },
+  // ];
+
+  // const curveWheelDots1 = wheelDots1.map(dots => getCurveDots(dots[0], dots[1], dots[2]));
+  // const curveWheelDots2 = wheelDots2.map(dots => getCurveDots(dots[0], dots[1], dots[2]));
+
+  // const testDots = getCurveDots(dots[0], dots[1], dots[2]);
+
+  // ctx.beginPath();
+  // curveWheelDots1.map(build(ctx));
+  // curveWheelDots2.map(build(ctx));
+
+  // build(ctx, dots);
+  // build(ctx, testDots);
+  // ctx.stroke();
 };
 
 window.onload = () => {
