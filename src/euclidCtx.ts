d7fb673d8ed1225207b2a3991e41eea814ff72c4
Vitalii Shapoval <vitalii.shapoval@starnavi.io>
2020-04-05 19:32:59 +0300

    fix: final refactoring

--- src/euclidCtx.ts
+++ src/euclidCtx.ts
@@ -6,198 +6,13 @@ import {
   drawDividerXLines,
   drawDividerYLines,
   build,
-  getArcDots,
-  getDotCoords,
-  degToRadian,
 } from './utils';
 import { euclidTransformations } from './transformations/euclid';
 
 import {
-  Proportions,
   INITIAL_X,
   INITIAL_Y,
 } from './constants';
-
-const draw = (
-  smallRadius: number,
-  centerX: number,
-  centerY: number,
-  rotation: number,
-  rotationX: number,
-  rotationY: number,
-  ctx: CanvasRenderingContext2D,
-) => {
-  const R_SMALL = smallRadius * Proportions.R_SMALL;
-  const R_MEDIUM = R_SMALL * Proportions.R_MEDIUM;
-  const R_BIG = R_SMALL * Proportions.R_BIG;
-
-  ctx.beginPath();
-  ctx.lineWidth = 2;
-
-  const arcDots = getArcDots(
-    centerX,
-    centerY,
-    R_BIG,
-    0,
-    360,
-  );
-  const euclidArcDots = euclidTransformations(
-    arcDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidArcDots);
-
-  // centered
-  const centeredArcDots = getArcDots(
-    centerX,
-    centerY,
-    R_SMALL,
-    0,
-    360,
-  );
-  const euclidCenteredArcDots = euclidTransformations(
-    centeredArcDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidCenteredArcDots);
-
-  // top / bottom
-  const topArcDots = getArcDots(
-    centerX,
-    centerY - 2 * R_SMALL,
-    R_SMALL,
-    135,
-    405,
-  );
-  const euclidTopArcDots = euclidTransformations(
-    topArcDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidTopArcDots);
-
-  const bottomArcDots = getArcDots(
-    centerX,
-    centerY + 2 * R_SMALL,
-    R_SMALL,
-    -45,
-    225,
-  );
-  const euclidBottomArcDots = euclidTransformations(
-    bottomArcDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidBottomArcDots);
-
-  // rigth / left
-  const rightArcDots = getArcDots(
-    centerX + 2 * R_SMALL,
-    centerY,
-    R_SMALL,
-    -135,
-    135,
-  );
-  const euclidRightArcDots = euclidTransformations(
-    rightArcDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidRightArcDots);
-
-  const leftArcDots = getArcDots(
-    centerX - 2 * R_SMALL,
-    centerY,
-    R_SMALL,
-    45,
-    315,
-  );
-  const euclidLefttArcDots = euclidTransformations(
-    leftArcDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidLefttArcDots);
-
-  // square
-  const squareDots = [
-    {
-      x: centerX + R_SMALL * 2,
-      y: centerY,
-    },
-    {
-      x: centerX,
-      y: centerY + R_SMALL * 2,
-    },
-    {
-      x: centerX - R_SMALL * 2,
-      y: centerY,
-    },
-    {
-      x: centerX,
-      y: centerY - R_SMALL * 2,
-    },
-    {
-      x: centerX + R_SMALL * 2,
-      y: centerY,
-    },
-  ];
-  const euclidSquareDots = euclidTransformations(
-    squareDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidSquareDots);
-
-  // medium
-  const bigRightCircleCoords = getDotCoords(
-    centerX,
-    centerY,
-    R_BIG,
-    -45,
-  );
-  const bigLeftCircleCoords = getDotCoords(
-    centerX,
-    centerY,
-    R_BIG,
-    225,
-  );
-  const bigRightCircleDots = getArcDots(
-    bigRightCircleCoords.x -
-      R_MEDIUM * Math.cos(degToRadian(45)),
-    bigRightCircleCoords.y +
-      R_MEDIUM * Math.sin(degToRadian(45)),
-    R_MEDIUM,
-    225,
-    405,
-  );
-  const bigLeftCircleDots = getArcDots(
-    bigLeftCircleCoords.x +
-      R_MEDIUM * Math.cos(degToRadian(45)),
-    bigLeftCircleCoords.y +
-      R_MEDIUM * Math.cos(degToRadian(45)),
-    R_MEDIUM,
-    135,
-    315,
-  );
-  const euclidBigRightCircleDots = euclidTransformations(
-    bigRightCircleDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  const euclidBigLeftCircleDots = euclidTransformations(
-    bigLeftCircleDots,
-    { x: rotationX, y: rotationY, angle: rotation },
-    null,
-  );
-  build(ctx, euclidBigRightCircleDots);
-  build(ctx, euclidBigLeftCircleDots);
-
-  ctx.stroke();
-  ctx.lineWidth = 1;
-};
 
 interface Dot {
   x: number;
@@ -235,22 +50,6 @@ const animate = (intitial: Dot, end: Dot) => {
   }
 
   return dots;
-};
-
-const drawPath = (
-  dots: any,
-  centerX: number,
-  centerY: number,
-  rotation: number,
-  rotationX: number,
-  rotationY: number,
-  ctx: CanvasRenderingContext2D,
-) => {
-  ctx.beginPath();
-  ctx.lineWidth = 2;
-
-  ctx.stroke();
-  ctx.lineWidth = 1;
 };
 
 const buildEuclid = () => {
@@ -312,72 +111,6 @@ const buildEuclid = () => {
     ...drawDividerXLines(canvasWidth),
     ...drawDividerYLines(canvasHeight),
   ];
-
-  // const arrA = [
-  //   [
-  //     { x: 167.7, y: 205.5 },
-  //     { x: 167.5, y: 175.7 },
-  //     { x: 161.4, y: 146.6 },
-  //   ],
-  //   [
-  //     { x: 179.3, y: 210.7 },
-  //     { x: 211.9, y: 210.7 },
-  //     { x: 244.6, y: 211.8 },
-  //   ],
-  //   [
-  //     { x: 244.6, y: 211.8 },
-  //     { x: 296.3, y: 213.7 },
-  //     { x: 347.4, y: 204.9 },
-  //   ],
-  //   [
-  //     { x: 347.4, y: 204.9 },
-  //     { x: 354.4, y: 195.6 },
-  //     { x: 362.4, y: 187 },
-  //   ],
-  //   [
-  //     { x: 362.4, y: 187 },
-  //     { x: 369.5, y: 170.7 },
-  //     { x: 364.3, y: 153.6 },
-  //   ],
-  // ].map(dots =>
-  //   dots.map(dot => ({
-  //     x: dot.x + INITIAL_X,
-  //     y: 500 - dot.y - INITIAL_Y,
-  //   })),
-  // );
-
-  // const arrB = [
-  //   [
-  //     { x: 160.8, y: 42.1 },
-  //     { x: 221.1, y: 29.3 },
-  //     { x: 282.7, y: 28.9 },
-  //   ],
-  //   [
-  //     { x: 4.2, y: 83.1 },
-  //     { x: 1.6, y: 93.6 },
-  //     { x: 8.9, y: 101.6 },
-  //   ],
-  //   [
-  //     { x: 8.9, y: 101.6 },
-  //     { x: 28.5, y: 111.7 },
-  //     { x: 48.2, y: 101.6 },
-  //   ],
-  //   [
-  //     { x: 48.2, y: 101.6 },
-  //     { x: 75, y: 86.8 },
-  //     { x: 98.4, y: 67 },
-  //   ],
-  //   [
-  //     { x: 98.4, y: 67 },
-  //     { x: 128.2, y: 48.1 },
-  //     { x: 162.8, y: 41.5 },
-  //   ],
-  // ].map(dots =>
-  //   dots.map(dot => ({
-  //     x: dot.x + INITIAL_X,
-  //     y: 500 - dot.y - INITIAL_Y,
-  //   })),
-  // );
 
   const arrA = [
     [
@@ -678,6 +411,11 @@ const buildEuclid = () => {
       { x: 350, y: 37 },
       { x: 393.5, y: 21 },
     ],
+    [
+      { x: 282.9, y: 289.4 },
+      { x: 350, y: 215 },
+      { x: 359.3, y: 119.9 },
+    ],
   ].map(dots =>
     dots.map(dot => ({
       x: dot.x + INITIAL_X,
@@ -688,27 +426,19 @@ const buildEuclid = () => {
   const curvedArrA = arrA.map(dots =>
     getCurveDots(dots[0], dots[1], dots[2]),
   );
-  const curvedArrB = arrB.map(dots =>
-    getCurveDots(dots[0], dots[1], dots[2]),
-  );
 
   ctx.beginPath();
   buildGrid(axesDots);
   ctx.strokeStyle = '#808080';
   buildGrid(dividerDots);
-  curvedArrA.map(build(ctx));
   ctx.stroke();
   ctx.strokeStyle = 'black';
 
-  // drawPath(
-  //   a,
-  //   +centerX.value,
-  //   +centerY.value,
-  //   +rotation.value,
-  //   +rotationX.value,
-  //   +rotationY.value,
-  //   ctx,
-  // );
+  ctx.beginPath();
+  ctx.lineWidth = 2;
+  curvedArrA.map(build(ctx));
+  ctx.stroke();
+  ctx.lineWidth = 1;
 
   let rotationState = 0;
 
@@ -733,14 +463,20 @@ const buildEuclid = () => {
     ctx.stroke();
     ctx.strokeStyle = 'black';
 
-    drawPath(
-      +centerX.value,
-      +centerY.value,
-      +rotation.value + rotationState,
-      +rotationX.value,
-      +rotationY.value,
-      ctx,
+    const config = {
+      x: +rotationX.value,
+      y: +rotationY.value,
+      angle: rotationState,
+    };
+    const transformed = curvedArrA.map(dots =>
+      euclidTransformations(dots, config, null),
     );
+
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    transformed.map(build(ctx));
+    ctx.stroke();
+    ctx.lineWidth = 1;
 
     rotationState += +rotation.value;
     rotation.value = '0';
@@ -798,14 +534,19 @@ const buildEuclid = () => {
     ctx.stroke();
     ctx.strokeStyle = 'black';
 
-    drawPath(
-      +inputX.value + x,
-      +inputY.value + y,
-      +rotation.value,
-      +rotationX.value,
-      +rotationY.value,
-      ctx,
+    const updatedDots = curvedArrA.map(dots =>
+      dots.map(dot => ({
+        x: dot.x + +centerX.value + +x,
+        y: dot.y + +centerY.value + +y,
+      })),
     );
+
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    updatedDots.map(build(ctx));
+    ctx.stroke();
+    ctx.lineWidth = 1;
+
     inputX.value = `${+centerX.value + x}`;
     inputY.value = `${+centerY.value + y}`;
 
@@ -814,16 +555,9 @@ const buildEuclid = () => {
   };
 
   animateInput.onclick = () => {
-    // console.log({ curvedA, curvedArrA });
-    // const test = curvedA.map((dot, i) => animate(dot, curvedB[i]));
-
-    const test = curvedArrA.map((dots, i) => {
-      return dots.map((dot, j) =>
-        animate(dot, curvedArrB[i][j]),
-      );
+    const test = arrA.map((dots, i) => {
+      return dots.map((dot, j) => animate(dot, arrB[i][j]));
     });
-
-    // console.log({ test, test1 });
 
     let iterator = 0;
     const i = setInterval(() => {
@@ -837,15 +571,16 @@ const buildEuclid = () => {
 
       ctx.beginPath();
       ctx.lineWidth = 2;
-      const buildArr = [];
 
       test.forEach(elem => {
-        for (let d = 0; d < 100; d += 1) {
-          buildArr.push(elem[d][iterator]);
-        }
+        const curvedArr = getCurveDots(
+          elem[0][iterator],
+          elem[1][iterator],
+          elem[2][iterator],
+        );
+        build(ctx, curvedArr);
       });
 
-      build(ctx, buildArr);
       ctx.stroke();
       ctx.lineWidth = 1;
 
